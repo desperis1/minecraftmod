a91dbf5c88a68df86a93ef654d71c73d1109c888
vvvami <Neryah@DESKTOP-K56A3FJ>
2021-02-22 12:11:40 +0200

    - Added White Neon

---
+++ src/main/java/net/mineon/block/WhiteNeonBlock.java
@@ -0,0 +1,57 @@
+
+package net.mineon.block;
+
+import net.mineon.MineonModElements;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import java.util.List;
+import java.util.Collections;
+
+@MineonModElements.ModElement.Tag
+public class WhiteNeonBlock extends MineonModElements.ModElement {
+	@ObjectHolder("mineon:white_neon")
+	public static final Block block = null;
+	public WhiteNeonBlock(MineonModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.GLASS).sound(SoundType.GLASS).hardnessAndResistance(1f, 10f).lightValue(15));
+			setRegistryName("white_neon");
+		}
+
+		@OnlyIn(Dist.CLIENT)
+		@Override
+		public boolean isEmissiveRendering(BlockState blockState) {
+			return true;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
